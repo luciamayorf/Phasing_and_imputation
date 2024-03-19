c0ac8cc18cc6eeb518363b77b483f4bedf207ff9
Lucía Mayor Fidalgo <96131721+luciamayorf@users.noreply.github.com>
2024-03-19 12:44:50 +0100

    Create chr_vcf_split.sh

---
+++ scripts/phasing/chr_vcf_split.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -e /mnt/lustre/scratch/nlsas/home/csic/eye/lmf/logs/phasing/slurm-%j.err
+#SBATCH -o /mnt/lustre/scratch/nlsas/home/csic/eye/lmf/logs/phasing/slurm-%j.out
+
+# This script splits a VCF file into single chromosomes using bedtools intersect.
+
+# Usage of the script: sbatch -c 5 --mem=10GB -t 01:00:00 chr_vcf_split.sh <input_vcf> <chr_list>
+
+# charge the bedtools module:
+module load cesga/2020 bedtools/2.31.0
+
+# define input vcf
+input_vcf=${1}
+
+# define output directory
+vcf_dir=$(dirname ${input_vcf})
+mkdir -p ${vcf_dir}/chr_vcfs
+
+# define input vcf basename
+vcf_basename=$(basename ${input_vcf} .vcf)
+
+# define list of chromosomes
+chr_list=${2}
+
+# split VCF into single chrs
+for chr in $(cut -f1 ${chr_list}) ; do
+    echo "extracting ${chr} from VCF"
+    chr_name=$(echo "${chr}" | cut -d'_' -f2-)
+    bedtools intersect -header -a ${input_vcf} -b <(grep -w "${chr}" ${chr_list}) > ${vcf_dir}/chr_vcfs/${vcf_basename}_${chr_name}.vcf
+done
