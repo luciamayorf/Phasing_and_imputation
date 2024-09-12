0a6e9c3952d3b36f1c5d9758771365cc544cefd1
Lucía Mayor Fidalgo <96131721+luciamayorf@users.noreply.github.com>
2024-09-12 16:37:04 +0200

    Update merge_all_autosomal_bcfs.sh

--- scripts/reference_validation/merge_all_autosomal_bcfs.sh
+++ scripts/reference_validation/merge_all_autosomal_bcfs.sh
@@ -8,13 +8,19 @@
 module load samtools
 
 # Define the input directory
-INPUT_DIR=/mnt/lustre/hsm/nlsas/notape/home/csic/ebd/jgl/lynx_genome/lynx_data/mLynPar1.2_ref_vcfs/novogene_lp_sept23/ref_panel_validation/GLIMPSE_ligate
+INPUT_DIR=/mnt/netapp2/Store_csebdjgl/lynx_genome/lynx_data/mLynPar1.2_ref_vcfs/novogene_lp_sept23/ref_panel_validation/GLIMPSE_ligate
 
 # Loop trough each coverage level to merge all the obtained BCF files
 for cov in 0_5 1 2; do
 
     # Merge all the final BCF files from the autosomes (excluding the X chromosome)
-    bcftools merge $(find ${INPUT_DIR}  -name "*_${cov}x_autosomes_merged.bcf") -O b -o ${INPUT_DIR}/c_lp_all_samples_${cov}x_autosomes_imputed.bcf
+    bcftools merge $(find ${INPUT_DIR}  -name "*_${cov}x_autosomes_merged.bcf") -O b -o ${INPUT_DIR}/${cov}_temp.vcf
+    
+    # Recalculate allele frequencies and the imputation INFO score
+    bcftools +impute-info -- ${INPUT_DIR}/${cov}_temp.vcf > ${INPUT_DIR}/${cov}_temp2.vcf
+    bcftools +fill-tags ${INPUT_DIR}/${cov}_temp2.vcf -- -t AF > ${INPUT_DIR}/c_lp_all_samples_${cov}x_autosomes_imputed.vcf
+
+   # Remove temporar vcf files
+   rm ${cov}_temp*.vcf 
 
 done
-
